762f5b961b0d13c23e2a4381003efc47efa2148c
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-12 01:27:59 +0500

    init db

---
+++ app/database/init_db.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import asyncio
+
+from app.database.db import (
+    engine,
+)
+
+from app.database.base import (
+    Base,
+)
+
+# IMPORT ALL MODELS
+from app.database.models import *
+
+
+async def init_models():
+
+    async with engine.begin() as conn:
+
+        await conn.run_sync(
+            Base.metadata.create_all
+        )
+
+
+if __name__ == "__main__":
+
+    asyncio.run(
+        init_models()
+    )
