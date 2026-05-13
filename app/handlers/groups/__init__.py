08aaee5da2b2d03845b08aa20ff1ca909e4f6c04
abdurahmon2909 <abdurahmon29091996@gmail.com>
2026-05-13 20:53:17 +0500

    start command

--- app/handlers/groups/__init__.py
+++ app/handlers/groups/__init__.py
@@ -1,7 +1,7 @@
-from app.handlers.groups.tracker import (
-    router as tracker_router,
-)
-
 from app.handlers.groups.auto_detect import (
     router as auto_detect_router,
+)
+
+from app.handlers.groups.tracker import (
+    router as tracker_router,
 )
